8685475d2bfd48ff2b5a37309a678a566ededffb
Bernarda Lusch <133001835+Berlusch@users.noreply.github.com>
2024-11-13 17:59:53 +0100

    Update Zoo.sql

--- Zoo.sql
+++ Zoo.sql
@@ -1,3 +1,17 @@
+-- Ovo je SQL skripta
+--prebacujem se na master bazu
+use master;
+go  
+--brišem bazu ako postoji
+drop database if exists edunovawp6;
+go
+
+create database edunovawp6;
+go
+
+use edunovawp6;
+go
+
 --create database zoo
 --use zoo
 
@@ -9,3 +23,21 @@ djelatnik int,
 prostorija int,
 datum int
 );
+
+create table prostorija(
+sifra int,
+dimenzije varchar(30),
+maksbroj int,
+mjesto varchar(30)
+);
+
+create table djelatnik(
+sifra int,
+ime varchar(50),
+prezime varchar(50),
+IBAN char(11)
+);
+
+create table datum(
+datumrodjenja datetime,
+datumdolaska datetime);
