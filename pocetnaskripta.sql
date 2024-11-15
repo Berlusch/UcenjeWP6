1debbf71b9fac2010c21af93cf40763f6bb0dd8d
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-11-15 08:15:49 +0100

    Create pocetnaskripta.sql

---
+++ pocetnaskripta.sql
@@ -0,0 +1,13 @@
+-- Ovo je SQL skripta
+--prebacujem se na master bazu
+use master;
+go  
+--brišem bazu ako postoji
+drop database if exists edunovawp6;
+go
+
+create database edunovawp6;
+go
+
+use edunovawp6;
+go
