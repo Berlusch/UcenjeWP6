a89c08348dba0d09d0036e00d8cf6183074311ab
Lea Bartos <lotalulea@gmail.com>
2024-11-23 10:11:39 +0100

    Update zavrsnirad i slika

--- zavrsnirad_aziludomime.sql
+++ zavrsnirad_aziludomime.sql
@@ -40,10 +40,10 @@ ime varchar(20) not null,
 datum_rodjenja date not null,
 spol varchar(10) not null,
 CONSTRAINT chk_spol CHECK (spol IN ('muški', 'ženski')),
-velicina int(10) not null references velicina(sifra),
-boja int(10) not null references boja(sifra),
+velicina int not null references velicina(sifra),
+boja int not null references boja(sifra),
 mojaprica varchar(300) not null,
-status int(10) not null references status(sifra),
+status int not null references status(sifra),
 kastracija bit not null,
 );
 
@@ -87,25 +87,26 @@ insert into boja(naziv) values
 ('šareni');
 
 insert into psi(brojcipa, ime, datum_rodjenja, spol, velicina, boja, mojaprica, kastracija, status) VALUES
-('HR123456789012345', 'Max', '2010-08-15', 'muški', 'srednji: 5-25 kg', 'bijeli', 'Max je energičan pas koji voli igru i šetnje.', 'da', 'slobodan'),
-('HR234567890123456', 'Luna', '2015-01-20', 'ženski', 'mali: do 5 kg', 'smeđi', 'Luna je mirna i nježna, voli biti u društvu ljudi.', 'da', 'slobodan'),
-('HR345678901234567', 'Leo', '2010-06-30', 'muški', 'veliki: >25 kg', 'crni', 'Leo je veliki pas koji traži puno prostora i igre.', 'da', 'slobodan'),
-('HR456789012345678', 'Bella', '2013-03-12', 'ženski', 'srednji: 5-25 kg', 'šareni', 'Bella je vesela i hrabra, uvijek spremna za avanture.', 'da', 'udomljen'),
-('HR567890123456789', 'Rex', '2011-11-09', 'muški', 'veliki: >25 kg', 'crni', 'Rex je zaštitnički nastrojen pas, idealan za obitelj.', 'da', 'slobodan'),
-('HR678901234567890', 'Maja', '2017-02-05', 'ženski', 'mali: do 5 kg', 'bijeli', 'Maja je umiljata i voli biti u centru pažnje.', 'da', 'rezerviran'),
-('HR789012345678901', 'Bruno', '2012-07-22', 'muški', 'srednji: 5-25 kg', 'smeđi', 'Bruno je prijateljski nastrojen pas koji voli sve ljude.', 'ne', 'slobodan'),
-('HR890123456789012', 'Zara', '2016-04-11', 'ženski', 'mali: do 5 kg', 'smeđi', 'Zara je vesela i razigrana, često trči po dvorištu.', 'da', 'slobodan'),
-('HR901234567890123', 'Oscar', '2009-10-02', 'muški', 'veliki: >25 kg', 'crni', 'Oscar je smiren pas koji voli dugi odmor.', 'da', 'privremeni_smještaj'),
-('HR012345678901234', 'Nina', '2018-06-17', 'ženski', 'mali: do 5 kg', 'bijeli', 'Nina je ljubazna i voli društvo drugih pasa.', 'ne', 'udomljen'),
-('HR112345678901235', 'Dino', '2014-12-08', 'muški', 'srednji: 5-25 kg', 'smeđi', 'Dino je odan pas koji se često igra s djecom.', 'da', 'slobodan'),
-('HR223456789012346', 'Kira', '2013-09-03', 'ženski', 'srednji: 5-25 kg', 'šareni', 'Kira je sramežljiva, ali odana prijateljica.', 'da', 'slobodan'),
-('HR334567890123457', 'Toby', '2016-11-10', 'muški', 'mali: do 5 kg', 'crni', 'Toby je aktivan pas koji uživa u trčanju i lovu na lopticu.', 'ne', 'slobodan'),
-('HR445678901234568', 'Rita', '2012-03-25', 'ženski', 'srednji: 5-25 kg', 'bijeli', 'Rita je vesela, uvijek spremna za igru.', 'da', 'udomljen'),
-('HR556789012345679', 'Maks', '2011-07-15', 'muški', 'veliki: >25 kg', 'smeđi', 'Maks je veliki, ljubazan pas koji se voli opuštati.', 'da', 'slobodan'),
-('HR667890123456780', 'Fiona', '2016-05-09', 'ženski', 'srednji: 5-25 kg', 'crni', 'Fiona je smirena i vrlo pažljiva prema djeci.', 'ne', 'rezerviran'),
-('HR778901234567891', 'Gustav', '2013-04-17', 'muški', 'srednji: 5-25 kg', 'smeđi', 'Gustav je veselo naravnog temperamenta, pravi pas za obitelj.', 'da', 'slobodan'),
-('HR889012345678902', 'Vera', '2014-01-30', 'ženski', 'mali: do 5 kg', 'bijeli', 'Vera je nježna i voli biti u društvu svojih vlasnika.', 'da', 'slobodan'),
-('HR990123456789013', 'Vuk', '2010-09-05', 'muški', 'veliki: >25 kg', 'crni', 'Vuk je vrlo energičan, voli biti vani i trčati po prirodi.', 'ne', 'slobodan');
+('HR123456789012345', 'Max', '2010-08-15', 'muški', 2, 1, 'Max je energičan pas koji voli igru i šetnje.', 'da', 'slobodan'),
+('HR234567890123456', 'Luna', '2015-01-20', 'ženski', 1, 2, 'Luna je mirna i nježna, voli biti u društvu ljudi.', 'da', 'slobodan'),
+('HR345678901234567', 'Leo', '2010-06-30', 'muški', 3, 3, 'Leo je veliki pas koji traži puno prostora i igre.', 'da', 'slobodan'),
+('HR456789012345678', 'Bella', '2013-03-12', 'ženski', 2, 4, 'Bella je vesela i hrabra, uvijek spremna za avanture.', 'da', 'udomljen'),
+('HR567890123456789', 'Rex', '2011-11-09', 'muški', 3, 3, 'Rex je zaštitnički nastrojen pas, idealan za obitelj.', 'da', 'slobodan'),
+('HR678901234567890', 'Maja', '2017-02-05', 'ženski', 1, 1, 'Maja je umiljata i voli biti u centru pažnje.', 'da', 'rezerviran'),
+('HR789012345678901', 'Bruno', '2012-07-22', 'muški', 2, 2, 'Bruno je prijateljski nastrojen pas koji voli sve ljude.', 'ne', 'slobodan'),
+('HR890123456789012', 'Zara', '2016-04-11', 'ženski', 1, 2, 'Zara je vesela i razigrana, često trči po dvorištu.', 'da', 'slobodan'),
+('HR901234567890123', 'Oscar', '2009-10-02', 'muški', 3, 3, 'Oscar je smiren pas koji voli dugi odmor.', 'da', 'privremeni_smještaj'),
+('HR012345678901234', 'Nina', '2018-06-17', 'ženski', 1, 1, 'Nina je ljubazna i voli društvo drugih pasa.', 'ne', 'udomljen'),
+('HR112345678901235', 'Dino', '2014-12-08', 'muški', 2, 2, 'Dino je odan pas koji se često igra s djecom.', 'da', 'slobodan'),
+('HR223456789012346', 'Kira', '2013-09-03', 'ženski', 2, 4, 'Kira je sramežljiva, ali odana prijateljica.', 'da', 'slobodan'),
+('HR334567890123457', 'Toby', '2016-11-10', 'muški', 1, 3, 'Toby je aktivan pas koji uživa u trčanju i lovu na lopticu.', 'ne', 'slobodan'),
+('HR445678901234568', 'Rita', '2012-03-25', 'ženski', 2, 1, 'Rita je vesela, uvijek spremna za igru.', 'da', 'udomljen'),
+('HR556789012345679', 'Maks', '2011-07-15', 'muški', 3, 2, 'Maks je veliki, ljubazan pas koji se voli opuštati.', 'da', 'slobodan'),
+('HR667890123456780', 'Fiona', '2016-05-09', 'ženski', 2, 3, 'Fiona je smirena i vrlo pažljiva prema djeci.', 'ne', 'rezerviran'),
+('HR778901234567891', 'Gustav', '2013-04-17', 'muški', 2, 2, 'Gustav je veselo naravnog temperamenta, pravi pas za obitelj.', 'da', 'slobodan'),
+('HR889012345678902', 'Vera', '2014-01-30', 'ženski', 1, 1, 'Vera je nježna i voli biti u društvu svojih vlasnika.', 'da', 'slobodan'),
+('HR990123456789013', 'Vuk', '2010-09-05', 'muški', 3, 3, 'Vuk je vrlo energičan, voli biti vani i trčati po prirodi.', 'ne', 'slobodan');
+
 
 insert into udomitelji(ime, prezime, adresa, telefon, email)
 VALUES
