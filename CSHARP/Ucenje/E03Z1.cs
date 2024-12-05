489a16a378b124a95bfbea9af7c2e3f43b13fcbb
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-12-05 10:05:46 +0100

    Update E03Z1.cs

--- CSHARP/Ucenje/E03Z1.cs
+++ CSHARP/Ucenje/E03Z1.cs
@@ -1,6 +1,9 @@
 ﻿
 namespace Ucenje
+
+
 {
+      
 
     //Program od korisnika unosi dva cijela broja
     //Program ispisuje u novim redovima
@@ -11,5 +14,30 @@ namespace Ucenje
     //5. kvocijent prvog i drugog broja
     internal class E03Z1
     {
+
+        public static void Izvedi()
+    {
+        Console.Write("Unesi jedan cijeli broj: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Unesi drugi cijeli broj: ");
+        int b = int.Parse(Console.ReadLine());
+
+         //1
+         Console.WriteLine("Zbroj ova dva broja je {0}", a + b);
+         
+         //2
+         Console.WriteLine("Razlika drugog i prvog broja je {0}", b - a);
+
+         //3
+         Console.WriteLine("Umnožak ova dva broja je {0}", a * b);
+
+         //4
+         Console.WriteLine("Zbroj kvadrata ova dva broja je {0}", a * a + b * b);
+
+         //5
+         Console.WriteLine("Kvocijent prvog i drugog broja je {0}", a / b + ", s ostatkom " + a%b + ".");
+
+
     }
+}
 }
