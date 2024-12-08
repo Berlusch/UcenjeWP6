4ab6ffc9eb503924e86ac041080a0f3455f18988
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-12-08 10:40:46 +0100

    Update E04Z3.cs

--- CSHARP/Ucenje/E04Z3.cs
+++ CSHARP/Ucenje/E04Z3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,49 @@ namespace Ucenje
 
     internal class E04Z3
     {
+        public static void Izvedi()
+        {
+
+            //dva unesena broja
+
+            /*Console.Write("Unesi jedan cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Unesi drugi cijeli broj: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a < b)
+            {
+                Console.WriteLine("Manji broj je {0}", a);
+            }
+
+            else
+            {
+                Console.WriteLine("Manji broj je {0}", b);
+            }
+            */
+
+            //tri unesena broja
+
+            Console.Write("Unesi jedan cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Unesi drugi cijeli broj: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Unesi jedan cijeli broj: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("Najmanji broj je {0}", a);
+            }
+            if (b < a && b < c)
+            {
+                Console.WriteLine("Najmanji broj je {0}", b);
+            }
+            if (c < a && c < b)
+            {
+                Console.WriteLine("Najmanji broj je {0}", c);
+            }
+            
+        }
     }
 }
