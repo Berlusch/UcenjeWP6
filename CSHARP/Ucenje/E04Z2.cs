e2f849a089b441037174d2e39e0a98d06e3f0426
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-12-08 10:14:24 +0100

    Update E04Z2.cs

--- CSHARP/Ucenje/E04Z2.cs
+++ CSHARP/Ucenje/E04Z2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,9 @@ namespace Ucenje
 
             // sada dajte rješenje s inline if
 
+            Console.WriteLine(broj%2 == 0 ? "Paran" : "Neparan");
+
         }
+        
     }
 }
