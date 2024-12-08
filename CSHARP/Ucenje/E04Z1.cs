a855790ce81b44683a7502dc8349fed9a4f8fd24
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-12-08 09:33:13 +0100

    Update E04Z1.cs

--- CSHARP/Ucenje/E04Z1.cs
+++ CSHARP/Ucenje/E04Z1.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace Ucenje
 {
-<<<<<<< HEAD
+
 
     // Program od korisnika traži unos broj godina koje ima korisnik
     // Program ispisuje da li je korisnik punoljetna osoba ili ne
 
 
     // dodatno: ako je unos ispod nula godine ili iznad 112 godina ipisasti GREŠKA
-=======
+
     //Program od korisnika traži unos broj godina koje ima korisnik
     //Program ispisuje da li je korisnik punoljetna osoba ili ne
 
     //dodatno: ako je unos ispod nula godine ili iznad 112 godina ispisati GREŠKA
->>>>>>> 6b5bbcef1a2b67d556a568dff19e946cca422870
+
     internal class E04Z1
     {
         public static void Izvedi()
         {
-<<<<<<< HEAD
+
 
             //Console.WriteLine("Z1");
             Console.Write("Unesite godine: ");
