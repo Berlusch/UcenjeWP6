81e1daa73652dac81cb6288e42274a471f7fee37
Bernarda Lusch <bernarda.lusch@gmail.com>
2024-12-09 18:55:56 +0100

    Create E07Z2.cs

---
+++ CSHARP/Ucenje/E07Z2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E07Z2
+    {
+        //za dva unesena cijela broja ispisati sve brojeve između njih
+
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesi jedan cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi drugi cijeli broj: ");
+            int b = int.Parse(Console.ReadLine());
+
+        }
+               
+        
+        for (int i = a; i<b; i++)
+            {
+                Console.WriteLine(i);
+            }
+}
+
+        
+        
+
+
+
+        
+
+
