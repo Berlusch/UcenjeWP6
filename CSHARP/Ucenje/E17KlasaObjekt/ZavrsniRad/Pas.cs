258a1ebbdc0066b5ea3d09b06f042fb104339480
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-01-15 14:59:21 +0100

    Zavrsni rad

---
+++ CSHARP/Ucenje/E17KlasaObjekt/ZavrsniRad/Pas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.ZavrsniRad
+{
+    public class Pas
+    {
+        public int Sifra { get; set; }
+        public char BrojCipa { get; set; }
+        public string Ime { get; set; } = "";
+        public DateOnly DatumRodjenja { get; set; }
+        public string Spol { get; set; } = "";
+        public string Velicina { get; set; } = "";
+        public string Boja { get; set; } = "";
+        public bool Kastracija { get; set; }
+        public Status Status { get; set; } = new Status();
+    }
+}
---
+++ CSHARP/Ucenje/E17KlasaObjekt/ZavrsniRad/Upit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.ZavrsniRad
+{
+    public class Upit
+    {
+        public int Sifra { get; set; }
+        public Pas Pas { get; set; } =new Pas();
+        public Udomitelj Udomitelj { get; set; } = new Udomitelj();
+        public DateOnly DatumUpita { get; set; }
+        public string StatusUpita { get; set; } = "";
+        public Pas[]? Psi { get; set; } 
+        public Udomitelj[]? Udomitelji { get; set; }
+    }
+}
