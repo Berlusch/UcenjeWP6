65a548f5425d263d0db20ef56d246bbb596b1db2
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-01-28 12:56:54 +0100

    Create Program.cs

---
+++ CSHARP/EdunovaAPP/Program.cs
@@ -0,0 +1,39 @@
+using EdunovaAPP.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+// dodati ovu liniju za swagger
+builder.Services.AddSwaggerGen();
+
+//dodavanje konteksta baze
+
+builder.Services.AddDbContext<EdunovaContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext")); });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapOpenApi();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// dodati ove dvije linije za swagger
+app.UseSwagger();
+app.UseSwaggerUI(o =>
+{
+    o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled",true);
+});
+
+app.MapControllers();
+
+app.Run();
